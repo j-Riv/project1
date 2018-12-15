51b2ace30b3c7b742b814aa6337d27008450d000
Jose Rivera <jose.alfredo.rivera@gmail.com>
2018-12-14 17:44:52 -0800

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ var config = {
 // firebase.initializeApp(config);
 
 var uiConfig = {
-    'signInSuccessUrl': 'http://localhost/project/movie-finder/search.html',
+    'signInSuccessUrl': 'http://localhost/projects/project1/movie.html',
     'signInOptions': [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
@@ -24,12 +24,15 @@ var app = firebase.initializeApp(config);
 var auth = app.auth();
 var ui = new firebaseui.auth.AuthUI(auth);
 
-// ui.start('#firebaseui-auth-container', uiConfig);
-
-// check if user is signed in
-// recommended way to check for user
-// login page
+/**
+ * init for login page funcs
+ * checks if user is logged in
+ * if user logged in get user data - forward to search
+ * else generate sign in/sign up form & sign in with google
+ */
 function loginInit() {
+    // check if user is signed in
+    // recommended way to check for user
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
@@ -59,8 +62,13 @@ function loginInit() {
         }
     });
 }
-// search page
+
+/**
+ * init for search/movie page firebase funcs
+ * gets user data if user is signed in
+ */
 function searchInit() {
+    // add logout functionality
     var btn = $('#login-logout');
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
@@ -79,6 +87,10 @@ function searchInit() {
     });
 }
 
+/**
+ * displays users data
+ * @param {object} user - the user data object
+ */
 function userInfo(user) {
     var name, email, photoUrl, uid, emailVerified;
     if (user != null) {
@@ -102,6 +114,10 @@ function userInfo(user) {
     }
 }
 
+/**
+ * updates the users data
+ * @param {string} user - the user uid 
+ */
 function updateUserInfo(user) {
     user.updateProfile({
         displayName: "Jose A. Rivera",
@@ -117,6 +133,12 @@ function updateUserInfo(user) {
 // Get a reference to the database service
 var database = firebase.database();
 
+/**
+ * checks the database for the current users data
+ * gets data if it exists
+ * creates data if it doesn't
+ * @param {object} user - the user data object
+ */
 function checkDB(user) {
     database.ref('/users/' + user.uid).once('value', function(snapshot) {
             var data = snapshot.val();
@@ -132,17 +154,26 @@ function checkDB(user) {
             console.error("The read failed: " + errorObject.code);
         });
 }
-// save the user's profile into Firebase so we can list users,
-// use them in Security and Firebase Rules, and show profiles
+
+/**
+ * saves the users data to database for future use
+ * @param {object} user - the user data object
+ */
 // userId, name, email, imageUrl
 function writeUserData(user) {
+    // save the user's profile into Firebase so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
     database.ref('users/' + user.uid).set({
         email: user.email
             //some more user data
     });
 }
 
-// create new user with email & password
+/**
+ * creates new user with email & password
+ * @param {string} email - new users email
+ * @param {string} password - new users password
+ */
 function createUser(email, password) {
     console.log('user created');
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
@@ -153,8 +184,12 @@ function createUser(email, password) {
     });
 }
 
-// normal email & pass sign on
-function loginUser() {
+/**
+ * signs in user with email & password
+ * @param {string} email - users email
+ * @param {string} password - users password
+ */
+function loginUser(email, password) {
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -166,29 +201,17 @@ function loginUser() {
 $('#signOut').on('click', function() {
     signOut();
 });
-// signout 
+
+/**
+ * signs out current user from firebase only
+ * if user is signed in with google they will remain signed in to google
+ */
 function signOut() {
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
-        var url = 'http://localhost/project/movie-finder/index.html';
+        var url = 'http://localhost/project/project1/index.html';
         $(location).attr('href', url);
     }).catch(function(error) {
         // An error happened.
     });
 }
-
-function loader() {
-    if ($('.firebaseui-id-page-provider-sign-in').length) {
-        // kill loader
-        clearInterval();
-        $('#loader').remove();
-        $('#theForm').show();
-    } else {
-        // check again
-        window.setInterval(function() {
-            /// call your function here
-            loader();
-        }, 500);
-    }
-}
-// loader();
